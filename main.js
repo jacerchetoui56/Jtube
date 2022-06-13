d9e1a1e23c229343fd5068a17bdaf65efedad12f
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-06-13 15:49:56 +0100

    improving the search bar

--- main.js
+++ main.js
@@ -8,35 +8,37 @@ const exitshare = document.querySelectorAll(".exitshare")
 const sharebox = document.querySelector('.share')
 const exitvideo = document.querySelectorAll(".fa-angle-down")
 
-let totop=-1;
+let totop = -1;
 
-for(let i=0;i<sharebtn.length;i++){
-    sharebtn[i].addEventListener('click',(event)=>{
-    event.stopImmediatePropagation()
-    sharebox.classList.toggle("show")
-    page.classList.toggle("blur")
+for (let i = 0; i < sharebtn.length; i++) {
+    sharebtn[i].addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+        sharebox.classList.toggle("show")
+        page.classList.toggle("blur")
     })
 }
-for(let i=0;i<exitshare.length;i++){
-    exitshare[i].addEventListener('click',()=>{
-    sharebox.classList.remove("show")
-    page.classList.remove("blur")
+for (let i = 0; i < exitshare.length; i++) {
+    exitshare[i].addEventListener('click', () => {
+        sharebox.classList.remove("show")
+        page.classList.remove("blur")
     })
 }
 
-for(let i=0;i<card.length;i++){
-    card[i].addEventListener('mouseover',()=>{
-        if(!card[i].classList.contains("launch")) video[i].play()
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('mouseover', () => {
+        if (!card[i].classList.contains("launch")) video[i].play()
     })
 }
-for(let i=0;i<card.length;i++){
-    card[i].addEventListener('mouseleave',()=>{
-        if(!card[i].classList.contains("launch")) {video[i].pause()
-        video[i].currentTime = 0}
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('mouseleave', () => {
+        if (!card[i].classList.contains("launch")) {
+            video[i].pause()
+            video[i].currentTime = 0
+        }
     })
 }
-for(let i=0;i<laterbtn.length;i++){
-    laterbtn[i].addEventListener('click',(event)=>{
+for (let i = 0; i < laterbtn.length; i++) {
+    laterbtn[i].addEventListener('click', (event) => {
         event.stopImmediatePropagation()
         latericon[i].classList.remove("fa-clock")
         latericon[i].classList.add("fa-check")
@@ -45,10 +47,10 @@ for(let i=0;i<laterbtn.length;i++){
 }
 
 
-for(let i=0;i<card.length;i++){
-    card[i].addEventListener('click',()=>{
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', () => {
         video[i].autoplay = "autoplay"
-        for(let j=0;j<card.length;j++){
+        for (let j = 0; j < card.length; j++) {
             card[j].classList.add("nohover")
         }
         pauseOthers(i);
@@ -58,12 +60,12 @@ for(let i=0;i<card.length;i++){
         video[i].controls = "controls"
         video[i].muted = ""
         card[i].style.order = totop; totop--;
-        window.scrollTo({top : 0 , behavior : 'smooth'});
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     })
 }
-function pauseOthers(n){
-    for(let i=0;i<card.length;i++){
-        if(i!=n && card[i].classList.contains("launch")) {
+function pauseOthers(n) {
+    for (let i = 0; i < card.length; i++) {
+        if (i != n && card[i].classList.contains("launch")) {
             video[i].pause()
             video[i].controls = ""
             video[i].muted = "muted"
@@ -72,11 +74,11 @@ function pauseOthers(n){
     }
 }
 
-for(let i=0;i<exitvideo.length;i++){
-    exitvideo[i].addEventListener('click',(event)=>{
+for (let i = 0; i < exitvideo.length; i++) {
+    exitvideo[i].addEventListener('click', (event) => {
         event.stopImmediatePropagation()
         card[i].classList.remove('launch')
-        cancelHover() 
+        cancelHover()
         video[i].pause()
         video[i].currentTime = 0
         exitvideo[i].style.display = 'none'
@@ -87,17 +89,17 @@ for(let i=0;i<exitvideo.length;i++){
     })
 }
 
-function cancelHover(){
-    for(let j=0;j<card.length;j++){
-        if(!card[j].classList.contains("nohover")) card[j].classList.remove("nohover")
+function cancelHover() {
+    for (let j = 0; j < card.length; j++) {
+        if (!card[j].classList.contains("nohover")) card[j].classList.remove("nohover")
     }
 }
-function verifyHover(){
-    let verif=true;
-    for(let i=0;i<card.length;i++){
-        if(!card[i].classList.contains("nohover")) verif=false;
+function verifyHover() {
+    let verif = true;
+    for (let i = 0; i < card.length; i++) {
+        if (!card[i].classList.contains("nohover")) verif = false;
     }
-    if(verif) for(let i=0;i<card.length;i++) card[i].classList.remove("nohover")
+    if (verif) for (let i = 0; i < card.length; i++) card[i].classList.remove("nohover")
 }
 
 
@@ -105,42 +107,33 @@ const searchbar = document.querySelector(".searchbar input")
 const titles = document.querySelectorAll(".text h2")
 const noresult = document.querySelector(".noresult")
 
-searchbar.addEventListener("keydown",(event)=>{
+searchbar.addEventListener("input", (event) => {
     pauseOthers()
-    let no=0
     let text = searchbar.value.toLowerCase()
-    const key = event.key
-    if(text.length==0 && key <='z' && key >='a') text = key
-    else if(key <='z' && key >='a' && text.length!=0) text+=key
-    for(let i=0;i<card.length;i++){
-        if(!titles[i].textContent.toLowerCase().includes(text)) {
+    let results = 0
+    for (let i = 0; i < card.length; i++) {
+        if (!titles[i].textContent.toLowerCase().includes(text)) {
             card[i].style.display = 'none';
-             no++; 
+        }
+        else {
+            card[i].style.display = 'block';
+            results++
         }
     }
-    if(no==card.length) noresult.style.display = 'block'
-    if(event.key == "Backspace") 
-    {
-        text = text.slice(0,-1)        
-        for(i=0;i<card.length;i++){
-        if(!titles[i].textContent.toLowerCase().includes(text) ) card[i].style.display = 'none';
-         else{ card[i].style.display = 'block'; noresult.style.display = 'none'}
-    }
-}
-    if(text.length ==1 || text.length <=1) for(let i=0;i<card.length;i++){
-         card[i].style.display = 'block'
-    }
+    if (results == 0) noresult.style.display = 'block'
+    else noresult.style.display = 'none'
+
 })
 
 //!toggling dark mode-------------
 const darkmode = document.querySelector(".darkmode")
-const lightmode= document.querySelector(".lightmode")
+const lightmode = document.querySelector(".lightmode")
 const header = document.querySelector('header')
 console.log(lightmode)
-darkmode.addEventListener('click',()=>{
+darkmode.addEventListener('click', () => {
     header.classList.toggle("dark")
     document.body.classList.toggle("dark")
-    card.forEach((onecard)=>{onecard.classList.toggle("dark")})
+    card.forEach((onecard) => { onecard.classList.toggle("dark") })
     document.querySelector("footer").classList.toggle("dark")
     noresult.classList.toggle('dark')
     sharebox.classList.toggle('dark')
@@ -148,11 +141,11 @@ darkmode.addEventListener('click',()=>{
 
 
 //!to top button ----------
-window.onscroll = ()=>{
-    if(document.documentElement.scrollTop > 900) totopbtn.classList.add('show')
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 900) totopbtn.classList.add('show')
     else totopbtn.classList.remove('show')
 }
 const totopbtn = document.querySelector(".totopbtn")
-totopbtn.addEventListener('click',()=>{
-    window.scrollTo({top:0, behavior : "smooth"})
+totopbtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
 })
